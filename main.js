f3d6380294b6d1fe4772327db64691a7c7cb61f6
Cool-Vandit <73426806+Cool-Vandit@users.noreply.github.com>
2021-02-26 15:24:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,26 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(425,425);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',identifyPose);
 }
+
+function modelLoaded(){
+    console.log("posenet is initialized");
+}
+
 
 function draw(){
     image(video,0,0,425,425);
+}
+
+function identifyPose(results){
+    if(results.length> 0){
+        console.log(results);
+        console.log("x position = " + results[0].pose.nose.x);
+        console.log("y position = " + results[0].pose.nose.y);
+    }
 }
 
 function take_snapshot(){
